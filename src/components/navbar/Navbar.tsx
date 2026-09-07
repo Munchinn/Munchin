ab0a23b338b4651e93e7faf5e59f13dd449df108
himanshu-45naik <himanshugit45@gmail.com>
2026-09-07 23:54:40 +0530

    feat: navbar components

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar(){
+    return(
+        <div className="container">
+            <a href="#munchin">Munchin</a>
+            <a href="#product">Product</a>
+            <a href="#findyourpal">Find Your Pal</a>
+            <a href="#dishestoeat">Dishes to Eat</a>
+            <a href="#foodhunt">Food Hunt</a>
+            <a href="#signup">Sign Up</a>
+        </div>
+    )
+}
