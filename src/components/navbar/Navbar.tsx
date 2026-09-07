27a7b2809eb614089f9a2a1191d3583f6ee78d66
himanshu-45naik <himanshugit45@gmail.com>
2026-09-08 02:12:01 +0530

    Built the logo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,24 @@
 import "./Navbar.css";
 
-export default function Navbar(){
-    return(
+export default function Navbar() {
+    return (
         <div className="container">
-            <a href="#munchin">Munchin</a>
-            <a href="#product">Product</a>
-            <a href="#findyourpal">Find Your Pal</a>
-            <a href="#dishestoeat">Dishes to Eat</a>
-            <a href="#foodhunt">Food Hunt</a>
-            <a href="#signup">Sign Up</a>
+
+            <div className="logo">
+                <a>munchin</a>
+            </div>
+
+            <div className="nav-links">
+                <a href="#product">Product</a>
+                <a href="#findyourpal">Find Your Pal</a>
+                <a href="#dishestoeat">Dishes to Eat</a>
+                <a href="#foodhunt">Food Hunt</a>
+            </div>
+
+            <div className="signup">
+                <a href="#signup">Sign Up</a>
+            </div>
+
         </div>
-    )
+    );
 }
